6020a495d64a9de72789a38d6b74d50c7e8fe52b
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-11-14 20:54:09 +0500

    added contributions request

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const { Console } = require('console');
 const http = require('http');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const { on } = require('process');
 
 const PORT = 8000;
 
 http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, Node.js!');
+    res.setHeader("Content-Type", "text/html; charset=utf-8;");
+    res.writeHead(200);
+
+    if(req.url != null) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open('GET', 'https://github-contributions.now.sh/api/v1' + req.url, false);
+        xmlHttp.send(null);
+        res.end(xmlHttp.responseText);
+    }
 }).listen(PORT, () => console.log('Server has been started!'));
